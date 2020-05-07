264be18f715026638c223bdd56596e0a7bae6753
DiegoSalazar1998 <55150273+DiegoSalazar1998@users.noreply.github.com>
2020-05-07 08:13:50 -0600

    Algunas validaciones

--- app/src/main/java/com/example/calculadora/MainActivity.java
+++ app/src/main/java/com/example/calculadora/MainActivity.java
@@ -14,12 +14,12 @@ import java.util.ArrayList;
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     EditText pantalla;
     public double operando1, operando2, res;
-    int op;
+    int op, error;
 
-    public int idsBtns[] = {R.id.num0,R.id.num1,R.id.num2,R.id.num3,R.id.num4,R.id.num5,R.id.num6,R.id.num7,R.id.num8,R.id.num9,
-            R.id.division,R.id.multi,R.id.clear,R.id.delete,R.id.suma,R.id.resta,R.id.raiz,R.id.porcentaje,R.id.coma,R.id.parentesis,
-            R.id.seno,R.id.coseno,R.id.tangente,R.id.igual,R.id.invercoseno,R.id.inverseno,R.id.invertangente,R.id.off,
-            R.id.factorial,R.id.potencia};
+    public int idsBtns[] = {R.id.num0, R.id.num1, R.id.num2, R.id.num3, R.id.num4, R.id.num5, R.id.num6, R.id.num7, R.id.num8, R.id.num9,
+            R.id.division, R.id.multi, R.id.clear, R.id.delete, R.id.suma, R.id.resta, R.id.raiz, R.id.porcentaje, R.id.coma, R.id.parentesis,
+            R.id.seno, R.id.coseno, R.id.tangente, R.id.igual, R.id.invercoseno, R.id.inverseno, R.id.invertangente, R.id.off,
+            R.id.factorial, R.id.potencia};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,7 +28,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         pantalla = (EditText) findViewById(R.id.pantalla);
 
-        for(int i=0;i<30;i++){
+        error = 0;
+        for (int i = 0; i < 30; i++) {
             new Button(getApplicationContext());
             Button btn;
             btn = findViewById(idsBtns[i]);
@@ -41,124 +42,145 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         String captura;
 
         switch (v.getId()) {
-           case R.id.num0:
+            case R.id.num0:
                 captura = pantalla.getText().toString();
-                captura = captura+"0";
+                captura = captura + "0";
                 pantalla.setText(captura);
                 break;
             case R.id.num1:
                 captura = pantalla.getText().toString();
-                captura = captura+"1";
+                captura = captura + "1";
                 pantalla.setText(captura);
                 break;
             case R.id.num2:
                 captura = pantalla.getText().toString();
-                captura = captura+"2";
+                captura = captura + "2";
                 pantalla.setText(captura);
                 break;
             case R.id.num3:
                 captura = pantalla.getText().toString();
-                captura = captura+"3";
+                captura = captura + "3";
                 pantalla.setText(captura);
                 break;
             case R.id.num4:
                 captura = pantalla.getText().toString();
-                captura = captura+"4";
+                captura = captura + "4";
                 pantalla.setText(captura);
                 break;
             case R.id.num5:
                 captura = pantalla.getText().toString();
-                captura = captura+"5";
+                captura = captura + "5";
                 pantalla.setText(captura);
                 break;
             case R.id.num6:
                 captura = pantalla.getText().toString();
-                captura = captura+"6";
+                captura = captura + "6";
                 pantalla.setText(captura);
                 break;
             case R.id.num7:
                 captura = pantalla.getText().toString();
-                captura = captura+"7";
+                captura = captura + "7";
                 pantalla.setText(captura);
                 break;
             case R.id.num8:
                 captura = pantalla.getText().toString();
-                captura = captura+"8";
+                captura = captura + "8";
                 pantalla.setText(captura);
                 break;
             case R.id.num9:
                 captura = pantalla.getText().toString();
-                captura = captura+"9";
+                captura = captura + "9";
                 pantalla.setText(captura);
                 break;
             case R.id.division:
-                try{
-                    String aux = pantalla.getText().toString();
-                    operando1 = Double.parseDouble(aux);
-                }catch (NumberFormatException e){}
+                if (error >= 1) {
+                    Toast.makeText(this, "No puede seleccionar otra operacion", Toast.LENGTH_LONG).show();
+                } else {
+                    try {
+                        String aux = pantalla.getText().toString();
+                        operando1 = Double.parseDouble(aux);
+                    } catch (NumberFormatException e) {
+                    }
 
-                pantalla.setText("");
-                op=4;
+                    pantalla.setText("");
+                    op = 4;
+                    error++;
+                }
                 break;
             case R.id.multi:
-                try{
-                    String aux = pantalla.getText().toString();
-                    operando1 = Double.parseDouble(aux);
-                }catch (NumberFormatException e){}
+                if (error >= 1) {
+                    Toast.makeText(this, "No puede seleccionar otra operacion", Toast.LENGTH_LONG).show();
+                } else {
+                    try {
+                        String aux = pantalla.getText().toString();
+                        operando1 = Double.parseDouble(aux);
+                    } catch (NumberFormatException e) {
+                    }
 
-                pantalla.setText("");
-                op=3;
+                    pantalla.setText("");
+                    op = 3;
+                    error++;
+                }
                 break;
             case R.id.clear:
                 pantalla.setText("");
-                operando1=0.0;
-                operando2=0.0;
-                res=0.0;
+                operando1 = 0.0;
+                operando2 = 0.0;
+                res = 0.0;
+                error = 0;
                 break;
             case R.id.delete:
-               if(!pantalla.getText().toString().equals("")){
-                   pantalla.setText(pantalla.getText().subSequence(0, pantalla.getText().length()-1)+"");
-               }
+                if (!pantalla.getText().toString().equals("")) {
+                    pantalla.setText(pantalla.getText().subSequence(0, pantalla.getText().length() - 1) + "");
+                }
                 break;
             case R.id.suma:
-                try{
+                try {
                     String aux = pantalla.getText().toString();
                     operando1 = Double.parseDouble(aux);
-                }catch (NumberFormatException e){}
+                } catch (NumberFormatException e) {
+                }
 
-               pantalla.setText("");
-               op=1;
+                pantalla.setText("");
+                op = 1;
                 break;
             case R.id.resta:
-                try{
+                try {
                     String aux = pantalla.getText().toString();
                     operando1 = Double.parseDouble(aux);
-                }catch (NumberFormatException e){}
+                } catch (NumberFormatException e) {
+                }
 
                 pantalla.setText("");
-                op=2;
+                op = 2;
                 break;
             case R.id.potencia:
-                try{
-                    String aux = pantalla.getText().toString();
-                    operando1 = Double.parseDouble(aux);
-                }catch (NumberFormatException e){}
+                if (error >= 1) {
+                    Toast.makeText(this, "No puede seleccionar otra operacion", Toast.LENGTH_LONG).show();
+                } else {
+                    try {
+                        String aux = pantalla.getText().toString();
+                        operando1 = Double.parseDouble(aux);
+                    } catch (NumberFormatException e) {
+                    }
 
-                pantalla.setText("");
-                op=5;
+                    pantalla.setText("");
+                    op = 5;
+                    error++;
+                }
                 break;
             case R.id.porcentaje:
-                try{
+                try {
                     String aux = pantalla.getText().toString();
                     operando1 = Double.parseDouble(aux);
-                }catch (NumberFormatException e){}
-
+                } catch (NumberFormatException e) {
+                }
                 pantalla.setText("");
-                op=6;
+                op = 6;
                 break;
             case R.id.coma:
                 captura = pantalla.getText().toString();
-                captura = captura+".";
+                captura = captura + ".";
                 pantalla.setText(captura);
                 break;
             case R.id.parentesis:
@@ -167,92 +189,142 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 pantalla.setText(captura);*/
                 break;
             case R.id.seno:
-                try{
-                    String aux = pantalla.getText().toString();
-                    operando1 = Double.parseDouble(aux);
-                }catch (NumberFormatException e){}
+                if (error >= 1) {
+                    Toast.makeText(this, "No puede seleccionar otra operacion", Toast.LENGTH_LONG).show();
+                } else {
+                    try {
+                        String aux = pantalla.getText().toString();
+                        operando1 = Double.parseDouble(aux);
+                    } catch (NumberFormatException e) {
+                    }
 
-                pantalla.setText("Sin("+operando1+")");
-                op=8;
+                    pantalla.setText("Sin(" + operando1 + ")");
+                    op = 8;
+                    error++;
+                }
                 break;
             case R.id.coseno:
-                try{
-                    String aux = pantalla.getText().toString();
-                    operando1 = Double.parseDouble(aux);
-                }catch (NumberFormatException e){}
+                if (error >= 1) {
+                    Toast.makeText(this, "No puede seleccionar otra operacion", Toast.LENGTH_LONG).show();
+                } else {
+                    try {
+                        String aux = pantalla.getText().toString();
+                        operando1 = Double.parseDouble(aux);
+                    } catch (NumberFormatException e) {
+                    }
 
-                pantalla.setText("Cos("+operando1+")");
-                op=9;
+                    pantalla.setText("Cos(" + operando1 + ")");
+                    op = 9;
+                    error++;
+                }
                 break;
             case R.id.tangente:
-                try{
-                    String aux = pantalla.getText().toString();
-                    operando1 = Double.parseDouble(aux);
-                }catch (NumberFormatException e){}
+                if (error >= 1) {
+                    Toast.makeText(this, "No puede seleccionar otra operacion", Toast.LENGTH_LONG).show();
+                } else {
+                    try {
+                        String aux = pantalla.getText().toString();
+                        operando1 = Double.parseDouble(aux);
+                    } catch (NumberFormatException e) {
+                    }
 
-                pantalla.setText("Tan("+operando1+")");
-                op=10;
+                    pantalla.setText("Tan(" + operando1 + ")");
+                    op = 10;
+                    error++;
+                }
                 break;
             case R.id.igual:
                 try {
                     String aux2 = pantalla.getText().toString();
                     operando2 = Double.parseDouble(aux2);
-                }catch (NumberFormatException e){}
+                } catch (NumberFormatException e) {
+                }
                 pantalla.setText("");
 
                 LogicaNegocio ln = new LogicaNegocio();
-                res = ln.calculoIgual(op,operando1,operando2);
+                res = ln.calculoIgual(op, operando1, operando2);
 
-                pantalla.setText(""+res);
+                pantalla.setText("" + res);
                 operando1 = res;
+                error = 0;
                 break;
             case R.id.invercoseno:
-                try{
-                    String aux = pantalla.getText().toString();
-                    operando1 = Double.parseDouble(aux);
-                }catch (NumberFormatException e){}
+                if (error >= 1) {
+                    Toast.makeText(this, "No puede seleccionar otra operacion", Toast.LENGTH_LONG).show();
+                } else {
+                    try {
+                        String aux = pantalla.getText().toString();
+                        operando1 = Double.parseDouble(aux);
+                    } catch (NumberFormatException e) {
+                    }
 
-                pantalla.setText("Csc("+operando1+")");
-                op=11;
+                    pantalla.setText("Csc(" + operando1 + ")");
+                    op = 11;
+                    error++;
+                }
                 break;
             case R.id.inverseno:
-                try{
-                    String aux = pantalla.getText().toString();
-                    operando1 = Double.parseDouble(aux);
-                }catch (NumberFormatException e){}
+                if (error >= 1) {
+                    Toast.makeText(this, "No puede seleccionar otra operacion", Toast.LENGTH_LONG).show();
+                } else {
+                    try {
+                        String aux = pantalla.getText().toString();
+                        operando1 = Double.parseDouble(aux);
+                    } catch (NumberFormatException e) {
+                    }
 
-                pantalla.setText("Sec("+operando1+")");
-                op=12;
+                    pantalla.setText("Sec(" + operando1 + ")");
+                    op = 12;
+                    error++;
+                }
                 break;
             case R.id.invertangente:
-                try{
-                    String aux = pantalla.getText().toString();
-                    operando1 = Double.parseDouble(aux);
-                }catch (NumberFormatException e){}
+                if (error >= 1) {
+                    Toast.makeText(this, "No puede seleccionar otra operacion", Toast.LENGTH_LONG).show();
+                } else {
+                    try {
+                        String aux = pantalla.getText().toString();
+                        operando1 = Double.parseDouble(aux);
+                    } catch (NumberFormatException e) {
+                    }
 
-                pantalla.setText("Ctg("+operando1+")");
-                op=13;
+                    pantalla.setText("Ctg(" + operando1 + ")");
+                    op = 13;
+                    error++;
+                }
                 break;
             case R.id.off:
                 finish();
                 break;
             case R.id.factorial:
-                try{
-                    String aux = pantalla.getText().toString();
-                    operando1 = Double.parseDouble(aux);
-                }catch (NumberFormatException e){}
+                if (error >= 1) {
+                    Toast.makeText(this, "No puede seleccionar otra operacion", Toast.LENGTH_LONG).show();
+                } else {
+                    try {
+                        String aux = pantalla.getText().toString();
+                        operando1 = Double.parseDouble(aux);
+                    } catch (NumberFormatException e) {
+                    }
 
-                pantalla.setText("");
-                op=14;
+                    pantalla.setText("");
+                    op = 14;
+                    error++;
+                }
                 break;
             case R.id.raiz:
-                try{
-                    String aux = pantalla.getText().toString();
-                    operando1 = Double.parseDouble(aux);
-                }catch (NumberFormatException e){}
+                if (error >= 1) {
+                    Toast.makeText(this, "No puede seleccionar otra operacion", Toast.LENGTH_LONG).show();
+                } else {
+                    try {
+                        String aux = pantalla.getText().toString();
+                        operando1 = Double.parseDouble(aux);
+                    } catch (NumberFormatException e) {
+                    }
 
-                pantalla.setText("√ ("+operando1+")");
-                op=7;
+                    pantalla.setText("√ (" + operando1 + ")");
+                    op = 7;
+                    error++;
+                }
                 break;
         }
     }
